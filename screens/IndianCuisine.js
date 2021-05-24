66bf515aa40fc989fdc48fd49675614ed18cae33
Saadalvi92 <53086047+Saadalvi92@users.noreply.github.com>
2021-05-25 03:31:07 +0500

    Image fix

--- screens/IndianCuisine.js
+++ screens/IndianCuisine.js
@@ -17,13 +17,14 @@ let height = Dimensions.get('window').height;
 let width = Dimensions.get('window').width;
 function IndianCuisine(props) {
   const Cuisine = props.route.params;
-  data.append('cuisineId', Cuisine.id);
-  var config = {
-    method: 'post',
-    url: 'http://amcrecipes.suretostop.com/getCuisinCategories',
-    data: data,
-  };
+
   useEffect(() => {
+    data.append('cuisineId', Cuisine.id);
+    var config = {
+      method: 'post',
+      url: 'http://amcrecipes.suretostop.com/getCuisinCategories',
+      data: data,
+    };
     axios(config)
       .then(function (response) {
         console.log(response.data);
@@ -35,82 +36,65 @@ function IndianCuisine(props) {
   }, []);
   const [categories, setCategories] = useState();
   return (
-    <ScrollView>
+    <View style={styles.container}>
       {console.log(Cuisine.image)}
-      <View style={styles.container}>
-        <Text
-          style={{fontWeight: 'bold', fontSize: 25, marginTop: height * 0.05}}>
-          {Cuisine.title}
+      <Text
+        style={{fontWeight: 'bold', fontSize: 25, marginTop: height * 0.05}}>
+        {Cuisine.title}
+      </Text>
+      <Image
+        style={{width: '100%', marginTop: '100%'}}
+        backgroundColor="#000"
+        resizeMode="contain"
+        source={{uri: Cuisine.image}}
+      />
+      <TouchableOpacity
+        style={{
+          width: '60%',
+          height: 55,
+          borderRadius: 10,
+          marginLeft: width * -0.2,
+          marginTop: height * -0.1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#CB4E47',
+        }}>
+        <Text style={{color: 'white', fontWeight: '600', fontSize: 16}}>
+          About Indian food
         </Text>
-        <Image
-          style={{width: '100%', marginTop: '100%'}}
-          backgroundColor="#000"
-          resizeMode="contain"
-          source={{uri: Cuisine.image}}
-        />
-        <TouchableOpacity
-          style={{
-            width: '60%',
-            height: 55,
-            borderRadius: 10,
-            marginLeft: width * -0.2,
-            marginTop: height * -0.1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#CB4E47',
-          }}>
-          <Text style={{color: 'white', fontWeight: '600', fontSize: 16}}>
-            About Indian food
-          </Text>
-        </TouchableOpacity>
-        <Text
-          style={{
-            fontWeight: '600',
-            fontSize: 23,
-            marginTop: height * 0.05,
-            marginLeft: width * -0.2,
-            marginTop: height * 0.02,
-          }}>
-          Indian food categories
-        </Text>
-        <FlatList
-          data={categories}
-          renderItem={({item}) => (
-            // <TouchableOpacity
-            //   style={{
-            //     width: '60%',
-            //     height: 55,
-            //     backgroundColor: '#F0E68C',
-            //     marginLeft: width * -0.2,
-            //     marginTop: height * 0.02,
-            //     alignItems: 'center',
-            //     justifyContent: 'center',
-            //   }}
-            //   onPress={() => props.navigation.navigate('appitizerScreen')}>
-            //   <Text style={{fontWeight: 'bold'}}>{item.title}</Text>
-            // </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() =>
-                props.navigation.navigate('appitizerScreen', item)
-              }>
-              <View
-                style={{
-                  width: width,
-                  height: 55,
-                  backgroundColor: '#F0E68C',
-                  marginLeft: width * -0.2,
-                  marginTop: height * 0.02,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  borderRadius: 10,
-                }}>
-                <Text>{item.title} </Text>
-              </View>
-            </TouchableOpacity>
-          )}
-        />
-      </View>
-    </ScrollView>
+      </TouchableOpacity>
+      <Text
+        style={{
+          fontWeight: '600',
+          fontSize: 23,
+          marginTop: height * 0.05,
+          marginLeft: width * -0.2,
+          marginTop: height * 0.02,
+        }}>
+        Indian food categories
+      </Text>
+      <FlatList
+        data={categories}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate('appitizerScreen', item)}>
+            <View
+              style={{
+                width: width,
+                height: 55,
+                backgroundColor: '#F0E68C',
+                marginLeft: width * -0.2,
+                marginTop: height * 0.02,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+              }}>
+              <Text>{item.title} </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
   );
 }
 
